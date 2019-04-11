5995c77c84e94c8c067d2a776ec09b53cd9e9e60
ajestes7 <ajestes@umich.edu>
2019-04-11 14:41:14 -0400

    Added Admin

--- munchandgo/menu/admin.py
+++ munchandgo/menu/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from menu.models import Restaurant, Food, Ingredient, foodType, ingredientType, Cuisine, Allergen
+
+admin.site.register(Restaurant)
+admin.site.register(Food)
+admin.site.register(Ingredient)
+admin.site.register(foodType)
+admin.site.register(ingredientType)
+admin.site.register(Cuisine)
+admin.site.register(Allergen)
--- munchandgo/menu/models.py
+++ munchandgo/menu/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Restaurant(models.Model):
     """Model representing a restaurant."""
     name = models.CharField(max_length=200, help_text='Enter a restaurant (e.g. Waffle House)')
-    menu = models.ForeignKey('Food', on_delete=models.SET_NULL, null=True)
-    cuisine = models.ForeignKey('Cuisine', on_delete=models.SET_NULL, null=True)
-    summary = models.CharField(max_length=200, help_text='Enter a short description abou the restaurant (e.g. Quick late-night breakfast joint)')
+    menu = models.ManyToManyField('Food')
+    cuisine = models.ManyToManyField('Cuisine')
+    summary = models.CharField(max_length=200, help_text='Enter a short description about the restaurant (e.g. Quick late-night breakfast joint)')
     website = models.CharField(max_length=200, help_text='Enter a the official restaurant website (e.g. https://www.wafflehouse.com/)')
 
     def __str__(self):
@@ -16,8 +16,8 @@ class Restaurant(models.Model):
 class Food(models.Model):
     """Model representing a food item."""
     name = models.CharField(max_length=200, help_text='Enter the food name (e.g. McDouble)')
-    ingredients = models.ForeignKey('Ingredients', on_delete=models.SET_NULL, null=True)
-    allergens = models.ForeignKey('Allergens', on_delete=models.SET_NULL, null=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    allergens = models.ManyToManyField('Allergen')
     foodType = models.ForeignKey('foodType', on_delete=models.SET_NULL, null=True)
     
 
@@ -25,9 +25,9 @@ class Food(models.Model):
         """String for representing the Model object."""
         return self.name
 
-class Ingredients(models.Model):
-    """Model representing ingredients."""
-    name = models.CharField(max_length=200, help_text='Enter the food name (e.g. McDouble)')
+class Ingredient(models.Model):
+    """Model representing  an ingredient."""
+    name = models.CharField(max_length=200, help_text='Enter the ingredient name (e.g. Onion Powder)')
     ingredientType = models.ForeignKey('ingredientType', on_delete=models.SET_NULL, null=True)
     
 
@@ -55,16 +55,16 @@ class ingredientType(models.Model):
 
 class Cuisine(models.Model):
     """Model representing the cuisine."""
-    name = models.CharField(max_length=200, help_text='Enter the suisine (e.g. Mexican)')
+    name = models.CharField(max_length=200, help_text='Enter the cuisine (e.g. Mexican)')
     
 
     def __str__(self):
         """String for representing the Model object."""
         return self.name
 
-class Allergens(models.Model):
-    """Model representing any allergens, if any."""
-    name = models.CharField(max_length=200, help_text='Enter the allergens, if any (e.g. Nuts)')
+class Allergen(models.Model):
+    """Model representing an allergen, if any."""
+    name = models.CharField(max_length=200, help_text='Enter an allergen, if any (e.g. Nuts)')
     
 
     def __str__(self):
