925582445f8fe47d76bda1533e7e0e319afff137
ajestes7 <ajestes@umich.edu>
2019-04-13 18:43:31 -0400

    Added Index View

--- munchandgo/foodshop/urls.py
+++ munchandgo/foodshop/urls.py
@@ -20,8 +20,22 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+# Use include() to add paths from the catalog application 
+from django.urls import include
+from django.urls import path
+
+urlpatterns += [
+    path('menu/', include('menu.urls')),
+]
+
 #Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
 urlpatterns += [
-    path('', RedirectView.as_view(url='/menu/')),
+    path('', RedirectView.as_view(url='/menu/', permanent=True)),
 ]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- munchandgo/menu/views.py
+++ munchandgo/menu/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from menu.models import Restaurant, Food, Ingredient, foodType, ingredientType, Cuisine, Allergen
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_restaurant = Restaurant.objects.all().count()
+    num_food = Food.objects.all().count()
+    num_ingredient = Ingredient.objects.all().count()
+    num_foodType = foodType.objects.all().count()
+    num_ingredientType = ingredientType.objects.all().count()
+    num_cuisine = Cuisine.objects.all().count()
+    num_allergen = Allergen.objects.all().count()
+
+    
+    context = {
+        'num_restaurant': num_restaurant,
+        'num_food': num_food,
+        'num_ingredient': num_ingredient,
+        'num_foodType': num_foodType,
+        'num_ingredientType': num_ingredientType,
+        'num_cuisine': num_cuisine,
+        'num_allergen': num_allergen,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
