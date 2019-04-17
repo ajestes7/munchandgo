8bb27c0ba307bd653553a4159a2cdf68ea674c0a
ajestes7 <ajestes@umich.edu>
2019-04-17 18:18:38 -0400

    Production Ready

--- munchandgo/foodshop/settings.py
+++ munchandgo/foodshop/settings.py
@@ -20,10 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1__x&piraaob1am$$b$wqrxi)!ki8=m01f2_yl-a=sgtst&xii'
+# SECRET_KEY = '1__x&piraaob1am$$b$wqrxi)!ki8=m01f2_yl-a=sgtst&xii'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['*']
 
